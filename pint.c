66af61a37945a0afd7e5c2686662dad85921946f
AmmarAbdalFattah3 <ammmarabdalfattah251@gmail.com>
2022-10-08 14:46:40 -0700

    pint

---
+++ pint.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ * pint- a function that prints top element of the stack
+ * @h: double pointer to the linked list head
+ * @line_number: instruction line number
+ */
+void pint(stack_t **h, unsigned int line_number)
+{
+	stack_t *temp = *h;
+
+	if (*h == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	while (temp)
+	{
+		printf("%i\n", temp->n);
+		break;
+	}
+}
+
