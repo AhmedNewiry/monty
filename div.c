3c6b86b5685e15edfc289f28ce92382956e2ea46
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-08 14:28:17 -0700

    div

---
+++ div.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+/**
+ * _div- divids the second top element of the stack by the first top one.
+ * @head: double pointer to the linked list head
+ * @line_number: instruction line number
+ */
+
+
+void _div(stack_t **head, unsigned int line_number)
+{
+
+
+	int x;
+
+	if (stack_len(*head) < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*(head))->n == 0)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	x = (*(head))->next->n / (*(head))->n;
+	(*(head))->next->n = x;
+	pop(head, line_number);
+}
