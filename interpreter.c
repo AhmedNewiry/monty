564dc3e3afb16c94fe4a61cdfd519fd93cb8d165
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-08 14:32:52 -0700

    interpeter

---
+++ interpreter.c
@@ -0,0 +1,64 @@
+#include "monty.h"
+
+/**
+ * main- entery point
+ * @argc: number of command line arguments
+ * @argv: command line arguments
+ * Return: 0 if success
+ */
+
+
+int main(int argc, char **argv)
+{
+	char *buffer, **op_tokens = NULL;
+	stack_t *head = NULL;
+	FILE *f_ptr;
+
+	int j = 0;
+	size_t n = 0;
+	unsigned int line_number = 0;
+
+
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n"), exit(EXIT_FAILURE);
+	}
+	f_ptr = fopen(argv[1], "r");
+	if (f_ptr == NULL)
+	{
+		printf("Error: Can't open file %s\n", argv[1]), exit(EXIT_FAILURE);
+	}
+
+
+	while (getline(&buffer, &n, f_ptr) != -1)
+	{
+
+
+		line_number++;
+		op_tokens = mod_strtok(buffer, "$\n\t ");
+		j = 0;
+		op_handler(&head, op_tokens, line_number, j);
+
+	}
+	fclose(f_ptr), free(buffer), free_stack(head), free(op_tokens);
+	return (0);
+}
+
+
+
+void op_handler(stack_t **head, char **op_tokens, unsigned int l_n, int j)
+{
+	while (op_tokens[j])
+	{
+		if (strcmp(op_tokens[j], "push") == 0)
+		{
+			j++;
+			push(head, op_tokens[j], l_n);
+		}
+		else
+		{
+			stack_handler(op_tokens[j], head, l_n);
+		}
+		j++;
+	}
+}
