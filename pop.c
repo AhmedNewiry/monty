71de5dce67f83a5a0776c6c8aff1443fe4a05ea6
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-08 14:47:14 -0700

    pop

---
+++ pop.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+ * pop- a function that deletes the stack first node
+ * @h: double pointer to the linked list head
+ * @line_number: instruction line number
+ */
+void pop(stack_t **head, unsigned int line_number)
+{
+
+	stack_t *temp;
+
+
+	temp = *head;
+
+	if (!*head || !head)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+
+	*head = (*(head))->next;
+	if (temp->next)
+	{
+		(*(head))->prev = NULL;
+	}
+	free(temp);
+	return;
+}
