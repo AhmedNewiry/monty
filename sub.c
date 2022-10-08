8e893778cfb25366d943d76778ed39f5e8f2ff60
AmmarAbdalFattah3 <ammmarabdalfattah251@gmail.com>
2022-10-08 15:24:17 -0700

    sub

---
+++ sub.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+ * sub- subs the top two elements of the stack.
+ * @head: double pointer to the linked list head
+ * @line_number: instruction line number
+ */
+
+void sub(stack_t **head, unsigned int line_number)
+{
+
+
+	int x;
+
+	if (stack_len(*head) < 2)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	x = (*(head))->next->n - (*(head))->n;
+	(*(head))->next->n = x;
+	pop(head, line_number);
+}
