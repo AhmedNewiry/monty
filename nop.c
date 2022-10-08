63cffd4794350968d54fc5594ee9937f0699025e
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-08 14:44:12 -0700

    nop

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * nop- a function that doesnot do anything
+ * @h: double pointer to the linked list head
+ * @line_number: instruction line number
+ */
+void nop(stack_t **head, unsigned int  line_number)
+{
+	(void)head;
+	(void)line_number;
+}
