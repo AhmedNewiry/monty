3dfef6287412c5bcd58b32b28d6a27199586c64d
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-10-10 12:23:47 -0700

    fix bug

--- div.c
+++ div.c
@@ -20,7 +20,7 @@ void _div(stack_t **head, unsigned int line_number)
 
 	if ((*(head))->n == 0)
 	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		fprintf(stderr, "L%d: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
