e6906503c295c0c3e8bd7f1a5f35eedacbf11324
AmmarAbdalFattah3 <ammmarabdalfattah251@gmail.com>
2022-10-10 12:30:40 -0700

    mod

--- mod.c
+++ mod.c
@@ -15,7 +15,7 @@ void mod(stack_t **head, unsigned int line_number)
 	}
 	if ((*(head))->n == 0)
 	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		fprintf(stderr, "L%d: division by zero\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
